89b2a45647c086f7a4d281398b0ac99529a69676
karlos <komaetil2001@hotmail.com>
2012-07-17 22:38:13 +0200

    Added printr function to console code in order to show data with pres

--- ajax_save.php
+++ ajax_save.php
@@ -1,3 +1,19 @@
 <?php
+
+//injects a simple function to pre code
 $content = $_POST["content"];
+$pretty = "function printr(\$data, \$exit = TRUE) {
+  if (\$data) {
+    print '<pre>';
+    print_r(\$data);
+    print '</pre>';
+  }
+  if (\$exit) {
+    exit;
+  }
+}";
+
+if(stristr($content,"?>")) $content = str_replace("?>",$pretty." ?>",$content);
+else $content = $content.$pretty;    
+
 file_put_contents("php_files/php_temp.php",$content);
